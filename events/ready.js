c334b11a9296202c4d4a3bc71075889e5ef85071
Neamkl <42867586+Neamkl@users.noreply.github.com>
2019-01-23 16:48:04 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,3 +13,9 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
+
+
+bot.channels.get("536578880824279050").fetchMessage("https://discordapp.com/channels/509983808313884674/536578880824279050/537644597090975747")
+
+
+
