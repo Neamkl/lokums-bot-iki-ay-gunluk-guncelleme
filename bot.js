c21fbd95d166988317c518b1822a0012ab09c4d2
Neamkl <42867586+Neamkl@users.noreply.github.com>
2019-01-23 16:46:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,7 +155,11 @@ if (msg.content.toLowerCase().match(/(discord\.gg\/)|www.|https:|.tk|tr.gg|.com|
 
 
 
-
+client.on('messageReactionAdd', (reaction, user) => {
+if (reaction.emoji.name == "target") {
+client.guilds.get("509983808313884674").members.get(user.id).addRole("✰Kayıtlı Üye✰")
+}
+});
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 's!site') {
